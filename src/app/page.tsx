a0cf550d704c7b53abf7a18b7356bc96e886cedc
Ljupche Vasilev <ljupche.vasilev@gmail.com>
2025-08-03 17:29:39 +0100

    feat: list all books

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,56 @@
+"use client"
+
+import { Prisma } from "@/generated/prisma"
+import { useEffect, useState } from "react"
+
 export default function Home() {
+  const [error, setError] = useState("")
+  const [isLoading, setIsLoading] = useState(false)
+  const [books, setBooks] = useState<Prisma.BookSelect[]>([])
+
+  const fetchBooks = async () => {
+    setIsLoading(true)
+
+    try {
+      const response = await fetch("/api/books")
+      const responseBody = await response.json()
+
+      if (response.status !== 200) {
+        const { error } = responseBody
+
+        console.error(error)
+        setError(error)
+      } else {
+        setBooks(responseBody)
+      }
+    } catch (error) {
+      console.error(error)
+      setError(error as string)
+    }
+
+    setIsLoading(false)
+  }
+
+  useEffect(() => {
+    fetchBooks()
+  }, [])
   return (
     <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-      List of books
+      <h1>List of books</h1>
+
+      {error && <div style={{ color: "red" }}>{error}</div>}
+      {isLoading && <div>Loading...</div>}
+
+      {books.map((book, index) => (
+        <div key={index}>
+          <div>{book.title}</div>
+          <div>{book.author}</div>
+          <div>
+            <label htmlFor="bookRead">Read?</label>
+            <input id="bookRead" type="checkbox" checked={book.read ?? false} />
+          </div>
+        </div>
+      ))}
     </main>
   )
 }
