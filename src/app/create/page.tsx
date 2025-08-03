34744c1ccaeaffa620e115b00cab65ad5002860d
Ljupche Vasilev <ljupche.vasilev@gmail.com>
2025-08-03 17:18:38 +0100

    feat: create book form

---
+++ src/app/create/page.tsx
@@ -0,0 +1,50 @@
+"use client"
+import { useRef, useState } from "react"
+
+export default function CreateBook() {
+  const [isLoading, setIsLoading] = useState(false)
+  const [error, setError] = useState<string | null>(null)
+
+  const titleRef = useRef<HTMLInputElement>(null)
+  const authorRef = useRef<HTMLInputElement>(null)
+
+  const createBook = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+
+    const title = titleRef.current?.value
+    const author = authorRef.current?.value
+
+    if (!title || !author) {
+      setError("Title and author are required")
+      return
+    }
+
+    setIsLoading(true)
+    const response = await fetch("/api/books", {
+      method: "POST",
+      body: JSON.stringify({ title, author }),
+    })
+
+    if (response.status !== 201) {
+      const responseBody = await response.json()
+      const { error } = responseBody
+      console.error(error)
+      setError(error)
+    }
+
+    setIsLoading(false)
+  }
+
+  return (
+    <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+      <h1>Create Book</h1>
+
+      {error && <div style={{ color: "red" }}>{error}</div>}
+      <form onSubmit={createBook}>
+        <input type="text" name="title" placeholder="Title" ref={titleRef} />
+        <input type="text" name="author" placeholder="Author" ref={authorRef} />
+        <button type="submit">{isLoading ? "Loading..." : "Submit"}</button>
+      </form>
+    </main>
+  )
+}
